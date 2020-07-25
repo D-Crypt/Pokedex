bcc0f902a59a992f11b6746fa6145a3afaf2ed79
D-Crypt <danielvaughan54@gmail.com>
2020-07-25 14:54:13 +0100

    Change number variable to URL

--- app/src/main/java/com/example/pokedex/PokemonActivity.java
+++ app/src/main/java/com/example/pokedex/PokemonActivity.java
@@ -8,19 +8,19 @@ import android.widget.TextView;
 public class PokemonActivity extends AppCompatActivity {
     private TextView nameTextView;
     private TextView numberTextView;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pokemon);
 
-        String name = getIntent().getStringExtra("name");
-        int number = getIntent().getIntExtra("number", 0);
-
+        url = getIntent().getStringExtra("url");
         nameTextView = findViewById(R.id.pokemon_name);
         numberTextView = findViewById(R.id.pokemon_number);
+    }
 
-        nameTextView.setText(name);
-        numberTextView.setText(String.format("#%03d", number));
+    public void load() {
+
     }
 }
