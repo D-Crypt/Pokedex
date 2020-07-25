526f369f809b9dc70073d020111b240e00679944
D-Crypt <danielvaughan54@gmail.com>
2020-07-25 15:02:57 +0100

    Implement load method

--- app/src/main/java/com/example/pokedex/PokemonActivity.java
+++ app/src/main/java/com/example/pokedex/PokemonActivity.java
@@ -3,7 +3,17 @@ package com.example.pokedex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class PokemonActivity extends AppCompatActivity {
     private TextView nameTextView;
@@ -21,6 +31,27 @@ public class PokemonActivity extends AppCompatActivity {
     }
 
     public void load() {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray typeEntries = response.getJSONArray("types");
 
+                    for (int i = 0; i < typeEntries.length(); ++i) {
+                        JSONObject typeEntry = typeEntries.getJSONObject(i);
+                        int slot = typeEntry.getInt("slot");
+                        String type = typeEntry.getJSONObject("type").getString("name");
+                    }
+                }
+                catch (JSONException e) {
+                    Log.e("test", "Pokemon JSON error", e);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("test", "Pokemon details error");
+            }
+        });
     }
 }
